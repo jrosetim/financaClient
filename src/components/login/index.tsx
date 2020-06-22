bd355d599aa7cdd97591d527605cdf1d821ab802
Julio Cesar Rosetim <julio.rosetim@elotech.com.br>
2020-06-22 20:40:01 -0300

    busca de usuario da api

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,21 +1,46 @@
-import React from 'react'
+import React, {useState, ChangeEvent}  from 'react'
 import {FiLogIn} from 'react-icons/fi';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 import './styles.css';
 
 const Login: React.FC = () => {
+  const [userEmail, setUserEmail] = useState<String>("");
+  const [userData, setUserData] = useState({ 
+    userEmail: '',
+    userPassword: ''
+  })
+
   function handleLogin() {
     alert('Logado com sucesso!');
+    axios.get(`http://localhost:3333/users/${userEmail}`)
+    .then( response => {
+       const [ userEmail, userPassword ] = response.data;
+
+       setUserData({
+         userEmail,
+         userPassword
+       })
+    } 
+    )
+
+    console.log(userData);
   };
+
+  function setEmail(event : ChangeEvent<HTMLInputElement>){
+    const value = event.target.value;
+    
+    setUserEmail(event.target.value);
+  }
 
   return (
     <div className="box">
       <form className="form">
         <span className="text-center">login</span>
         <div className="input-container">
-          <input type="text" />
-          <label>Usuário</label>		
+          <input onChange={setEmail} type="text" />
+          <label >Usuário</label>		
         </div>
         <div className="input-container">		
           <input type="password" />
